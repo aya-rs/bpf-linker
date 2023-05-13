9899b4f1b974e7e237f09191c000388d20898915
Mike Rostecki <vadorovsky@protonmail.com>
2023-05-13 11:38:25 +0100

    di: Sanitize generic names in functions (subprograms)
    Previously we were sanitizing only struct types
    (`DW_tag_structure_type`). Here it's done also for functions
    (`DW_tag_subprogram`).
    
    This change also fixes the `sanitize_type_name` function to accept the
    `_` character. It's a valid character in C types and converting it to
    `_5F_` is not great for readability.

--- tests/assembly/di_generics.rs
+++ tests/assembly/di_generics.rs
@@ -23,5 +23,49 @@ struct Bar<T> {
 #[link_section = "maps"]
 static mut BAR: Bar<Foo<u32>> = Bar { x: Foo { x: 0 } };
 
+// NOTE(vadorovsky): I couldn't come up with any simpler example of function
+// with generic which wouldn't get inlined.
+
+#[no_mangle]
+#[link_section = "uprobe/connect"]
+pub fn connect() {
+    let _ = my_function(1, 2);
+    let _ = my_function(3, 4);
+    let _ = my_function(5, 6);
+    let _ = my_function(7, 8);
+}
+
+pub trait Add<Rhs = Self> {
+    type Output;
+
+    fn add(self, rhs: Rhs) -> Self::Output;
+}
+
+impl Add for u32 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        self + other
+    }
+}
+
+#[inline(never)]
+pub fn my_function<T: Add<Output = T> + Copy>(x: T, y: T) -> T {
+    x.add(y)
+        .add(x)
+        .add(y)
+        .add(x)
+        .add(y)
+        .add(x)
+        .add(y)
+        .add(x)
+        .add(y)
+        .add(x)
+        .add(y)
+        .add(x)
+        .add(y)
+}
+
 // CHECK: name: "Foo_3C_u32_3E_"
-// CHECK: name: "Bar_3C_di_5F_generics_3A__3A_Foo_3C_u32_3E__3E_"
+// CHECK: name: "Bar_3C_di_generics_3A__3A_Foo_3C_u32_3E__3E_"
+// CHECK: name: "my_function_3C_u32_3E_"
