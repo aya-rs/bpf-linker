c1716fb9c966b0e3a04c9997dd28589a9f7147eb
Tamir Duberstein <tamird@gmail.com>
2023-07-18 10:26:10 -0400

    log: append rather than truncate
    When passing `-Clink-arg=--log-file=path-to-file` to rustc through
    cargo, it's likely that the linker is going to be used multiple times
    concurrently.  This isn't ideal because the logs interleave; it would be
    better to change the flag semantics to --log-dir so that each linker
    instance can have its own log file, but that is left for another day.

--- src/bin/bpf-linker.rs
+++ src/bin/bpf-linker.rs
@@ -10,7 +10,7 @@ use simplelog::{
 };
 use std::{
     env,
-    fs::{self, File},
+    fs::{self, OpenOptions},
     path::PathBuf,
     str::FromStr,
 };
@@ -190,8 +190,8 @@ fn main() {
         _ => None,
     };
     let log_level = log_level.or(env_log_level).unwrap_or(LevelFilter::Warn);
-    if let Some(path) = log_file {
-        let log_file = match File::create(path) {
+    if let Some(log_file) = log_file {
+        let log_file = match OpenOptions::new().create(true).append(true).open(log_file) {
             Ok(f) => f,
             Err(e) => {
                 error(
