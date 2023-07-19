82378f9aebc11084006ee0758de7e0af37b50119
Tamir Duberstein <tamird@gmail.com>
2023-07-19 15:21:28 -0400

    Make iterators a little more paranoid

--- src/llvm/iter.rs
+++ src/llvm/iter.rs
@@ -4,67 +4,81 @@ use llvm_sys::core::*;
 use llvm_sys::prelude::*;
 
 macro_rules! llvm_iterator {
-    ($trait_name:ident, $iterator_name:ident, $iterable:ty, $method_name:ident, $item_ty:ty, $first:expr, $next:expr) => {
+    ($trait_name:ident, $iterator_name:ident, $iterable:ty, $method_name:ident, $item_ty:ty, $first:expr, $last:expr, $next:expr $(,)?) => {
         pub trait $trait_name {
             fn $method_name(&self) -> $iterator_name;
         }
 
-        impl $trait_name for $iterable {
-            fn $method_name(&self) -> $iterator_name {
-                $iterator_name {
-                    iterable: PhantomData,
-                    next: unsafe { $first(*self) },
-                }
-            }
+        pub struct $iterator_name<'a> {
+            lifetime: PhantomData<&'a $iterable>,
+            next: $item_ty,
+            last: $item_ty,
         }
 
-        pub struct $iterator_name<'a> {
-            iterable: PhantomData<&'a $iterable>,
-            next: $item_ty,
+        impl $trait_name for $iterable {
+            fn $method_name(&self) -> $iterator_name {
+                let first = unsafe { $first(*self) };
+                let last = unsafe { $last(*self) };
+                assert_eq!(first.is_null(), last.is_null());
+                $iterator_name {
+                    lifetime: PhantomData,
+                    next: first,
+                    last,
+                }
+            }
         }
 
         impl<'a> Iterator for $iterator_name<'a> {
             type Item = $item_ty;
 
             fn next(&mut self) -> Option<Self::Item> {
-                let next = self.next;
-                if !next.is_null() {
-                    self.next = unsafe { $next(next) };
-                    Some(next)
-                } else {
-                    None
+                let Self {
+                    lifetime: _,
+                    next,
+                    last,
+                } = self;
+                if next.is_null() {
+                    return None;
                 }
+                let last = *next == *last;
+                let item = *next;
+                *next = unsafe { $next(*next) };
+                assert_eq!(next.is_null(), last);
+                Some(item)
             }
         }
     };
 }
 
-llvm_iterator!(
-    IterModuleFunctions,
-    FunctionsIter,
-    LLVMModuleRef,
-    functions_iter,
-    LLVMValueRef,
-    LLVMGetFirstFunction,
-    LLVMGetNextFunction
-);
-
-llvm_iterator!(
+llvm_iterator! {
     IterModuleGlobals,
     GlobalsIter,
     LLVMModuleRef,
     globals_iter,
     LLVMValueRef,
     LLVMGetFirstGlobal,
-    LLVMGetNextGlobal
-);
+    LLVMGetLastGlobal,
+    LLVMGetNextGlobal,
+}
 
-llvm_iterator!(
+llvm_iterator! {
     IterModuleGlobalAliases,
-    GlobalAliasessIter,
+    GlobalAliasesIter,
     LLVMModuleRef,
     global_aliases_iter,
     LLVMValueRef,
     LLVMGetFirstGlobalAlias,
-    LLVMGetNextGlobalAlias
-);
+    LLVMGetLastGlobalAlias,
+    LLVMGetNextGlobalAlias,
+}
+
+llvm_iterator! {
+    IterModuleFunctions,
+    FunctionsIter,
+    LLVMModuleRef,
+    functions_iter,
+    LLVMValueRef,
+    LLVMGetFirstFunction,
+    LLVMGetLastFunction,
+    LLVMGetNextFunction,
+}
