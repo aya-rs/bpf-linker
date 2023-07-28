7d34053df5f84d6455f914fd3d7c481fb97882c0
Tamir Duberstein <tamird@gmail.com>
2023-07-28 13:40:53 -0400

    Add btf test example
    Requires a compiletest-rs release containing
    https://github.com/Manishearth/compiletest-rs/pull/278.

---
+++ tests/btf/assembly/basic.rs
@@ -0,0 +1,18 @@
+// assembly-output: bpf-linker
+// no-prefer-dynamic
+// compile-flags: --crate-type bin -C link-arg=--emit=obj -C debuginfo=2 -C lto=true
+
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+#[link_section = "uprobe/connect"]
+pub fn connect() {}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// We check the BTF dump out of bpftool
+// CHECK: FUNC 'connect' type_id=1 linkage=global
