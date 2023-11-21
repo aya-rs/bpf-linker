728ea00a19ddb8cc046d8394feffeba02f3773b2
Michal Rostecki <vadorovsky@protonmail.com>
2023-11-22 00:35:17 +0100

    di: Sanitize data carrying enums correctly
    Data carrying enums are causing a crash of the BPF Assembly Printer
    pass. As a workaround, we were always replacing the elements of that
    enum metadata with an empty MDNode.
    
    However, #142 broke this behavior by calling `replace_elements` on a
    wrong composite type - it was calling it on the inner one (data
    carrying field), instead of the outer one (enum). This change fixes
    it.

---
+++ tests/btf/assembly/data-carrying-enum.rs
@@ -0,0 +1,43 @@
+// assembly-output: bpf-linker
+// compile-flags: --crate-type cdylib -C link-arg=--emit=obj -C debuginfo=2
+
+#![no_std]
+
+pub enum SimpleEnum {
+    First,
+    Second,
+    Third,
+}
+
+pub enum DataCarryingEnum {
+    First { a: u32, b: i32 },
+    Second(u32, i32),
+    Third(u32),
+}
+
+#[no_mangle]
+pub static A: SimpleEnum = SimpleEnum::First;
+#[no_mangle]
+pub static B: SimpleEnum = SimpleEnum::Second;
+#[no_mangle]
+pub static C: SimpleEnum = SimpleEnum::Third;
+
+#[no_mangle]
+pub static X: DataCarryingEnum = DataCarryingEnum::First { a: 54, b: -23 };
+#[no_mangle]
+pub static Y: DataCarryingEnum = DataCarryingEnum::Second(54, -23);
+#[no_mangle]
+pub static Z: DataCarryingEnum = DataCarryingEnum::Third(36);
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// The data-carrying enum should be not included in BTF.
+
+// CHECK: ENUM 'SimpleEnum' encoding=UNSIGNED size=1 vlen=3
+// CHECK-NEXT: 'First' val=0
+// CHECK-NEXT: 'Second' val=1
+// CHECK-NEXT: 'Third' val=2
+// CHECK-NOT: ENUM 'DataCarryingEnum'
