223f8540e80c75730d95d925d6ab9e03cfbfa011
Tamir Duberstein <tamird@gmail.com>
2025-04-01 10:19:23 -0400

    Appease `invalid_null_arguments`
    This was promoted from a clippy lint in nightly.
    
    Link: https://github.com/rust-lang/rust/pull/119220

--- tests/assembly/auxiliary/dep-exports.rs
+++ tests/assembly/auxiliary/dep-exports.rs
@@ -4,7 +4,7 @@
 
 pub fn dep_public_symbol() -> u8 {
     // read_volatile stops LTO inlining the function in the calling crate
-    unsafe { core::ptr::read_volatile(0 as *const u8) }
+    unsafe { core::ptr::read_volatile(core::ptr::dangling()) }
 }
 
 #[no_mangle]
