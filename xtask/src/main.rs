4e3b342b821b19992a60fdd5d839cc2ef2102a15
Michal R <vad.sol@proton.me>
2025-10-22 11:15:48 +0200

    xtask: Add `build-llvm` command
    To allow easy builds of LLVM locally, move the logic of fetching the
    sources and executing CMake from the workflow YAML to an xtask command.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,7 +1,21 @@
-use std::{env, ffi::OsString, os::unix::ffi::OsStringExt as _, path::PathBuf, process::Command};
+use std::{
+    env,
+    ffi::OsString,
+    fs,
+    io::{BufRead as _, BufReader, Read as _},
+    os::unix::ffi::OsStringExt as _,
+    path::{Path, PathBuf},
+    process::{Command, Stdio},
+    thread,
+    time::Duration,
+};
 
 use anyhow::{Context as _, Result, bail};
+use flate2::read::GzDecoder;
+use indicatif::{ProgressBar, ProgressStyle};
 use rustc_build_sysroot::{BuildMode, SysrootConfig, SysrootStatus};
+use tar::Archive;
+use walkdir::WalkDir;
 
 #[derive(Clone, clap::ValueEnum)]
 enum Target {
@@ -24,11 +38,50 @@ struct BuildStd {
     target: Target,
 }
 
+#[derive(clap::Parser)]
+struct DownloadLlvmSources {
+    /// GitHub repository to download the sources from.
+    #[arg(long, default_value = "aya-rs/llvm-project")]
+    git_repo: String,
+    /// Git ref (branch, commit, tag) to fetch.
+    #[arg(long, default_value = "rustc/21.1-2025-08-01")]
+    git_ref: String,
+    /// Directory to extract the sources to.
+    #[arg(long)]
+    src_dir: PathBuf,
+}
+
+#[derive(clap::Parser)]
+struct BuildLlvm {
+    /// Source directory.
+    #[arg(long)]
+    src_dir: PathBuf,
+    /// Directory in which the built LLVM artifacts are installed.
+    #[arg(long)]
+    install_prefix: PathBuf,
+}
+
+#[derive(clap::Subcommand)]
+enum LlvmSubcommand {
+    /// Downloads LLVM sources from GitHub.
+    DownloadSources(DownloadLlvmSources),
+    /// Builds LLVM.
+    Build(BuildLlvm),
+}
+
+#[derive(clap::Parser)]
+struct Llvm {
+    #[command(subcommand)]
+    subcommand: LlvmSubcommand,
+}
+
 #[derive(clap::Subcommand)]
 enum XtaskSubcommand {
     /// Builds the Rust standard library for the given target in the current
     /// toolchain's sysroot.
     BuildStd(BuildStd),
+    /// Manages and builds LLVM.
+    Llvm(Llvm),
 }
 
 /// Additional build commands for bpf-linker.
@@ -85,9 +138,212 @@ fn build_std(options: BuildStd) -> Result<()> {
     Ok(())
 }
 
+fn progress_bar_spinner(template: &str) -> ProgressBar {
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(
+        ProgressStyle::default_spinner()
+            .template(template)
+            .expect("failed to create a progress bar with template")
+            .tick_strings(&["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"]),
+    );
+    pb.enable_steady_tick(Duration::from_millis(100));
+    pb
+}
+
+fn unpack_llvm_sources(src_dir: &Path, data: Vec<u8>) -> Result<()> {
+    let gz_decoder = GzDecoder::new(&data[..]);
+    let mut archive = Archive::new(gz_decoder);
+
+    let pb = progress_bar_spinner("{spinner:.green} [{elapsed_precise}] unpacking...");
+    fs::create_dir_all(src_dir)?;
+    for entry_result in archive.entries()? {
+        let mut entry = entry_result?;
+        let path = entry.path()?;
+
+        if let Some(stripped_path) = path.components().skip(1).collect::<PathBuf>().into() {
+            if stripped_path.as_os_str().is_empty() {
+                continue;
+            }
+
+            let dest_path = src_dir.join(stripped_path);
+            let _unpacked = entry.unpack(dest_path)?;
+        }
+    }
+    pb.finish_with_message("Unpacking completed");
+
+    Ok(())
+}
+
+fn download_llvm_sources(options: DownloadLlvmSources) -> Result<()> {
+    let DownloadLlvmSources {
+        git_repo,
+        git_ref,
+        src_dir,
+    } = options;
+
+    let client = reqwest::blocking::Client::new();
+
+    let url = format!("https://github.com/{git_repo}/archive/refs/heads/{git_ref}.tar.gz");
+    let mut response = client
+        .get(&url)
+        .send()
+        .with_context(|| format!("failed to sent a GET request to {url}"))?;
+
+    if !response.status().is_success() {
+        anyhow::bail!("HTTP error: {}", response.status());
+    }
+
+    let pb = progress_bar_spinner("{spinner:.green} [{elapsed_precise}] {bytes} downloaded...");
+    let mut downloaded: u64 = 0;
+    let mut buf = vec![0; 8192];
+    let mut data = Vec::new();
+    loop {
+        let bytes_read = response
+            .read(&mut buf)
+            .with_context(|| format!("failed to read response to a request to {url}"))?;
+        if bytes_read == 0 {
+            break;
+        }
+        data.extend_from_slice(&buf[..bytes_read]);
+        downloaded += bytes_read as u64;
+        pb.set_position(downloaded);
+    }
+    pb.finish_with_message("Download completed");
+
+    unpack_llvm_sources(&src_dir, data)
+}
+
+fn execute_command(cmd: &mut Command) -> Result<()> {
+    let cmd = cmd.stdout(Stdio::piped()).stderr(Stdio::piped());
+    let mut child = cmd
+        .spawn()
+        .with_context(|| format!("failed to spawn command {cmd:?}"))?;
+    let stdout = child
+        .stdout
+        .take()
+        .unwrap_or_else(|| panic!("expected piped stdout in command {cmd:?}"));
+    let stderr = child
+        .stderr
+        .take()
+        .unwrap_or_else(|| panic!("expected piped stderr in command {cmd:?}"));
+    thread::scope(|s| {
+        let _stdout_handle = s.spawn(|| {
+            for line in BufReader::new(stdout).lines() {
+                let line = line.unwrap_or_else(|e| {
+                    panic!("failed to retrieve stdout line from command {cmd:?}: {e:?}")
+                });
+                println!("{line}");
+            }
+        });
+        let _stderr_handle = s.spawn(|| {
+            for line in BufReader::new(stderr).lines() {
+                let line = line.unwrap_or_else(|e| {
+                    panic!("failed to retrieve stderr line from command {cmd:?}: {e:?}")
+                });
+                eprintln!("{line}")
+            }
+        });
+    });
+
+    Ok(())
+}
+
+/// Moves targets over the symlinks that point to them.
+///
+/// This whole dance would be simpler if CMake supported
+/// `CMAKE_INSTALL_MODE=MOVE`.
+fn move_absolute_symlink_targets(install_prefix: &Path) -> Result<()> {
+    for entry in WalkDir::new(install_prefix)
+        .follow_links(false)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().is_symlink())
+    {
+        let link_path = entry.path();
+        let target = fs::read_link(link_path)
+            .with_context(|| format!("failed to read the link {}", link_path.display()))?;
+        if target.is_absolute() {
+            // Move the file the link points to into the location of the symlink
+            fs::rename(&target, link_path).with_context(|| {
+                format!(
+                    "failed to move the target file {} to the location of the symlink {}",
+                    target.display(),
+                    link_path.display()
+                )
+            })?;
+        }
+    }
+
+    Ok(())
+}
+
+fn build_llvm(options: BuildLlvm) -> Result<()> {
+    let BuildLlvm {
+        src_dir,
+        install_prefix,
+    } = options;
+
+    let build_dir = src_dir.join("build");
+
+    let mut install_arg = OsString::from("-DCMAKE_INSTALL_PREFIX=");
+    install_arg.push(install_prefix.as_os_str());
+    let mut cmake_configure = Command::new("cmake");
+    let cmake_configure = cmake_configure
+        .arg("-S")
+        .arg(src_dir.join("llvm"))
+        .arg("-B")
+        .arg(&build_dir)
+        .args([
+            "-G",
+            "Ninja",
+            "-DCMAKE_BUILD_TYPE=RelWithDebInfo",
+            "-DCMAKE_C_COMPILER=clang",
+            "-DCMAKE_CXX_COMPILER=clang++",
+            "-DLLVM_BUILD_LLVM_DYLIB=ON",
+            "-DLLVM_ENABLE_ASSERTIONS=ON",
+            "-DLLVM_ENABLE_PROJECTS=",
+            "-DLLVM_ENABLE_RUNTIMES=",
+            "-DLLVM_INSTALL_UTILS=ON",
+            "-DLLVM_LINK_LLVM_DYLIB=ON",
+            "-DLLVM_TARGETS_TO_BUILD=BPF",
+            "-DLLVM_USE_LINKER=lld",
+        ])
+        .arg(install_arg);
+    println!("Configuring LLVM with command {cmake_configure:?}");
+    execute_command(cmake_configure)?;
+
+    let mut cmake_build = Command::new("cmake");
+    let cmake_build = cmake_build
+        .arg("--build")
+        .arg(build_dir)
+        .args(["--target", "install"])
+        // Create symlinks rather than copies to conserve disk space,
+        // especially on GitHub-hosted runners.
+        //
+        // Since the LLVM build creates a bunch of symlinks (and this setting
+        // does not turn those into symlinks-to-symlinks), use absolute
+        // symlinks so we can distinguish the two cases.
+        .env("CMAKE_INSTALL_MODE", "ABS_SYMLINK");
+    println!("Building LLVM with command {cmake_build:?}");
+    execute_command(cmake_build)?;
+
+    move_absolute_symlink_targets(&install_prefix)?;
+
+    Ok(())
+}
+
+fn llvm(options: Llvm) -> Result<()> {
+    let Llvm { subcommand } = options;
+    match subcommand {
+        LlvmSubcommand::DownloadSources(options) => download_llvm_sources(options),
+        LlvmSubcommand::Build(options) => build_llvm(options),
+    }
+}
+
 fn main() -> Result<()> {
     let CommandLine { subcommand } = clap::Parser::parse();
     match subcommand {
         XtaskSubcommand::BuildStd(options) => build_std(options),
+        XtaskSubcommand::Llvm(options) => llvm(options),
     }
 }
