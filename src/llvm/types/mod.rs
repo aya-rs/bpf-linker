2ed8455c5ac03494d4480308495cb069bfaf5f77
Michal R <vad.sol@proton.me>
2025-11-05 13:39:17 +0100

    di: Add `di-sanitizer` feature, allowing to disable DI sanitization
    Starting from LLVM 21.1.5, the only necessary fixup is the making sure
    that the type names are C-compatible and don't contain characters like
    `<`, `>`. That should be fixed in the kernel. Phasing out this behavior
    will take time and effort, but ability to disable fixups will make the
    development efforts easier.

--- src/llvm/types/mod.rs
+++ src/llvm/types/mod.rs
@@ -1,5 +1,7 @@
 pub(super) mod context;
+#[cfg(feature = "di-sanitizer")]
 pub(super) mod di;
+#[cfg(feature = "di-sanitizer")]
 pub(super) mod ir;
 pub(super) mod memory_buffer;
 pub(super) mod module;
