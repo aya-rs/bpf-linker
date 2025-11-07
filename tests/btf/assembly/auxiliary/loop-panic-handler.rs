2a0aa56f2f8031efc0aab7adadbfda315c4f350e
Michal R <vad.sol@proton.me>
2025-11-07 09:13:05 +0100

    tests: Symlink `loop-panic-handler.rs` to `btf/`
    `assembly/auxiliary/loop-panic-handler.rs` and
    `btf/assembly/auxiliary/loop-panic-handler.rs` had exactly the same
    content. Instead of keeping copies, make a symlink.

--- tests/btf/assembly/auxiliary/loop-panic-handler.rs
+++ tests/btf/assembly/auxiliary/loop-panic-handler.rs
@@ -1,8 +1,1 @@
-// no-prefer-dynamic
-// compile-flags: --crate-type rlib
-#![no_std]
-
-#[panic_handler]
-fn panic_impl(_: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+../../../assembly/auxiliary/loop-panic-handler.rs
