2958b2c87c8c9a8ac40f6acc82b44390ad3ddebc
David Zbarsky <dzbarsky@gmail.com>
2026-06-14 21:00:28 -0400

    Revert "tests: simplify invalid input path comparison"

--- tests/link_ir.rs
+++ tests/link_ir.rs
@@ -3,7 +3,7 @@
     reason = "Cargo exposes package-wide dependencies"
 )]
 
-use std::path::Path;
+use std::ffi::OsStr;
 
 fn create_test_ir_content(name: &str) -> String {
     format!(
@@ -76,7 +76,9 @@ fn test_link_ir_files() {
                 bpf_linker::OutputType::Object,
                 Vec::<&str>::new(),
             ),
-            Err(bpf_linker::LinkerError::InvalidInputType(path)) if path == Path::new("in_memory::corrupted.ll")
+            // TODO(MSRV 1.91.0): peel away the `AsRef::<OsStr>::as_ref`.
+            // `PathBuf` implements `PartialEq<str>` starting in Rust 1.91.0.
+            Err(bpf_linker::LinkerError::InvalidInputType(path)) if AsRef::<OsStr>::as_ref(&path) == "in_memory::corrupted.ll"
         );
     }
 }
