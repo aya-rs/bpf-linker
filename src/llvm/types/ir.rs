681bdc52e51d37768ada05332452c3a5b834d86b
vad <vad.sol@proton.me>
2026-08-06 23:49:49 +0200

    chore: Remove LLVM 20 support
    Rust 1.90.0 is the newest toolchain that uses LLVM 20. The newest Rust
    version in general is 1.97.1. There is no need to keep it.

--- src/llvm/types/ir.rs
+++ src/llvm/types/ir.rs
@@ -165,7 +165,6 @@ impl Metadata<'_> {
                 | LLVMMetadataKind::LLVMDIGenericSubrangeMetadataKind
                 | LLVMMetadataKind::LLVMDIArgListMetadataKind
                 | LLVMMetadataKind::LLVMDIAssignIDMetadataKind => Metadata::Other(value),
-                #[cfg(not(feature = "llvm-20"))]
                 LLVMMetadataKind::LLVMDISubrangeTypeMetadataKind
                 | LLVMMetadataKind::LLVMDIFixedPointTypeMetadataKind => Metadata::Other(value),
             }
